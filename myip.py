8b6b2598a6644b4c2851cc7f157b504189d2b39e
Thomas Kwon <tdkwon@gmail.com>
2013-10-30 15:35:21 +0000

    added logfile for myip.py

--- myip.py
+++ myip.py
@@ -1,5 +1,8 @@
+import sendalert
 import urllib2
 import re
+import os
+import datetime
 
 my_url = 'http://www.whatismyip.com/ip-tools/ip-address-lookup/'
 response = urllib2.urlopen(my_url)
@@ -19,10 +22,21 @@ try:
 except IOError:
 	old_ip = "0.0.0.0"
 
+try:
+	logfile = open('myip.log', 'a')
+except IOError:
+	print "Failed to open myip.log"	
+
+todaysdate = str(datetime.date.today())
 
 if old_ip == new_ip:
 	print "no change"
+	logfile.write(todaysdate + " no change")
 else:
 	ofp = open('ipfile.txt', 'w')
 	ofp.write(new_ip)
 	print old_ip, ' changed to ', new_ip
+	subject = "Your IP has been changed to: " + new_ip
+	body = old_ip + " has been changed to " + new_ip
+	sendalert.sendalert(subject, body)
+	
